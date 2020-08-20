41c4e8a6c532e3221e063a1e5fe1a6cb04da34f3
Elham Fabe <elhamfabe@gmail.com>
2020-08-19 21:23:06 -0400

    app.js and todo.txt

--- Develop/app.js
+++ Develop/app.js
@@ -10,12 +10,56 @@ const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
 
+//  Ask how many members are in this team
+function EmployeeChoice() {
+    inquirer
+        .prompt([
+            {
+                type: "confirm",
+                name: "addEmployee",
+                message: "Would you like to add an Employee?"
+
+            },
+            {
+                type: "list",
+                name: "employeeRole",
+                message: "What is the employee's role?",
+                when: input => input.addEmployee === true,
+                choices: [
+                    "Manager",
+                    "Engineer",
+                    "Intern"
+                ]
+            }
+        ]).then(answers => {
+            console.log(answers);
+            switch (answers.EmployeeRole) {
+                case "Manager":
+                    managerQuestions();
+                    break;
+                case "Engineer":
+                    engineerQuestions();
+                    break;
+                case "Intern":
+                    internQuestions();
+                    break;
+                
+            }
+            switch (answers.addEmployee){
+                case false
+                const html = render(EmployeeArr)
+                 
+            }
+        })
+
+}
+EmployeeChoice()
 
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
 const EmployeeArr = []
 
-function ManagerQuestions() {
+function managerQuestions() {
     inquirer
         .prompt([
             {
@@ -40,13 +84,85 @@ function ManagerQuestions() {
                 message: "Enter your manager's office number:"
             },
         ]).then(function (answers) {
-            const manager = new manager(answers.name, parseInt(answers.id), answers.email, parseInt(answers.office));
+            const manager = new Manager(answers.name, parseInt(answers.id), answers.email, parseInt(answers.office));
             EmployeeArr.push(manager);
-            addMember();
+            EmployeeChoice()
+            console.log(manager);
 
         });
-        console.log(manager);
+
+
 }
+
+function engineerQuestions() {
+    inquirer
+        .prompt([
+            {
+                type: "input",
+                name: "name:",
+                message: "Enter Engineer's name:",
+
+            },
+            {
+                type: "input",
+                name: "id",
+                message: "Enter your Engineer's id:"
+            },
+            {
+                type: "input",
+                name: "email",
+                message: "Enter your Engineer's email:"
+            },
+            {
+                type: "input",
+                name: "Github",
+                message: "Enter your Github user name:"
+            },
+        ]).then(function (answers) {
+            const engineer = new Engineer(answers.name, parseInt(answers.id), answers.email, answers.github);
+            EmployeeArr.push(engineer);
+            EmployeeChoice()
+            console.log(engineer);
+        });
+
+
+
+}
+function internQuestions() {
+    inquirer
+        .prompt([
+            {
+                type: "input",
+                name: "name:",
+                message: "Enter intern's name:",
+
+            },
+            {
+                type: "input",
+                name: "id",
+                message: "Enter your intern's id:"
+            },
+            {
+                type: "input",
+                name: "email",
+                message: "Enter your intern's email:"
+            },
+            {
+                type: "input",
+                name: "School",
+                message: "Enter your intern's school:"
+            },
+        ]).then(function (answers) {
+            const intern = new Intern(answers.name, parseInt(answers.id), answers.email,answers.school);
+            EmployeeArr.push(intern);
+            EmployeeChoice()
+            console.log(intern);
+
+        });
+
+        
+}
+ 
 // After the user has input all employees desired, call the `render` function (required
 // above) and pass in an array containing all employee objects; the `render` function will
 // generate and return a block of HTML including templated divs for each employee!
