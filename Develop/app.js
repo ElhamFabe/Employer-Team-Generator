36fc5b1a614716fba895461c2e1d310c203385b6
Elham Fabe <elhamfabe@gmail.com>
2020-08-22 16:16:24 -0400

    Assets folder and css

--- Develop/app.js
+++ Develop/app.js
@@ -36,22 +36,7 @@ function EmployeeChoice() {
             }
         ]).then(answers => {
             console.log(answers);
-           
-            // HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
-            // and Intern classes should all extend from a class named Employee; see the directions
-            // for further information. Be sure to test out each class and verify it generates an
-            // object with the correct structure and methods. This structure will be crucial in order
-            // for the provided `render` function to work! ```
-
-            // After the user has input all employees desired, call the `render` function (required
-            // above) and pass in an array containing all employee objects; the `render` function will
-            // generate and return a block of HTML including templated divs for each employee!
-
-            // After you have your html, you're now ready to create an HTML file using the HTML
-            // returned from the `render` function. Now write it to a file named `team.html` in the
-            // `output` folder. You can use the variable `outputPath` above target this location.
-            // Hint: you may need to check if the `output` folder exists and create it if it
-            // does not.
+          
             switch (answers.addEmployee) {
                 case false:
                     const html = render(EmployeeArr);
@@ -86,9 +71,6 @@ function EmployeeChoice() {
         });
 }
 EmployeeChoice()
-
-// Write code to use inquirer to gather information about the development team members,
-// and to create objects for each team member (using the correct classes as blueprints!)
 // create team employee array
 const EmployeeArr = []
 
@@ -132,9 +114,7 @@ function managerQuestions() {
 
         });
 }
-// HINT: each employee type (manager, engineer, or intern) has slightly different
-// information; write your code to ask different questions via inquirer depending on
-// employee type.
+
 function engineerQuestions() {
     inquirer
         .prompt([
